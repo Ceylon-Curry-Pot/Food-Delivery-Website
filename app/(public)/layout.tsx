798e8046c61d2075910a38b4e1c68c1ed77a60c8
Sheran21 <samarejanula@gmail.com>
2026-07-26 14:40:27 +0530

    added loyalty page to app and layout

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,16 +1,14 @@
 import Navbar from '@/components/navbar/Navbar';
 import Footer from '@/components/global/Footer';
+import LoyaltyModal from '@/components/loyalty/LoyaltyModal';
 
-export default function PublicLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
       {children}
       <Footer />
+      <LoyaltyModal />
     </>
   );
 }
